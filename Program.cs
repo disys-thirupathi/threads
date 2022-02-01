24a7f9a210b3d0b61c0a3e3aa15eccdbe87380d0
thirupathi <thirupathi.narayanan@disys.com>
2022-02-01 17:21:14 +0530

    threads

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace threads
+{
+    public class Threadmethods
+    {
+        public void methods()
+        {
+           for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void method1()
+        {
+            Console.WriteLine("2nd thread");
+            int i = 9;
+            while (i >= 0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+        }
+    }
+    internal class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            Threadmethods tm = new Threadmethods();
+            Thread th = new Thread(new ThreadStart(tm.methods));
+            Thread th2 = new Thread(new ThreadStart(tm.method1));
+            Console.WriteLine("Thread start method and state method " + th.ThreadState);
+            th.Start();
+            Console.WriteLine("Thread suspend method which suspend the object "+th.ThreadState);
+            th.Suspend();
+            Console.WriteLine("Resuming the suspended state "+th.ThreadState);
+            th.Resume();
+            Console.WriteLine("joining threads " + th.ThreadState);
+            Console.WriteLine(th.ThreadState);
+            th.Join();
+            th2.Start();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ multithread.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+namespace multithread
+{
+    public class multithread
+    {
+        public static void thread1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("#1 : " + i);
+                if (i == 5)
+                {
+                    Console.WriteLine("Sleeping for 5 seconds");
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+        public static void thread2()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("2#  " + i);
+            }
+        }
+    }
+    public class program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread t1 = new Thread(multithread.thread1);
+            Thread t2 = new Thread(multithread.thread2);
+            t1.Start();
+            t2.Start();
+            Console.ReadLine();
+
+        }
+
+    }
+}
